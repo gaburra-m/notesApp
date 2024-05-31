10e231b62db78f71bb6ccef12c41fbf070abd017
Gaburra <gaburra.m@gmail.com>
2024-05-31 10:55:21 -0600

    rutas auth y sign-up html

---
+++ src/app/auth/features/auth-log-in/auth-log-in.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-log-in',
+  standalone: true,
+  imports: [],
+  templateUrl: './auth-log-in.component.html',
+  styles: ``,
+})
+export default class AuthLogInComponent {}
---
+++ src/app/auth/features/auth-sign-up/auth-sign-up.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-sign-up',
+  standalone: true,
+  imports: [],
+  templateUrl: './auth-sign-up.component.html',
+  styles: ``,
+})
+export default class AuthSignUpComponent {}
