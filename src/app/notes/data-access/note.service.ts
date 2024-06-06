3fc032ceb89d055bbf48d319b22c3bfe2e6caf00
Gaburra <gaburra.m@gmail.com>
2024-06-05 18:48:38 -0600

    Note service

---
+++ src/app/notes/data-access/note.service.ts
@@ -0,0 +1,61 @@
+import { Injectable, computed, inject, signal } from '@angular/core';
+import { SupabaseService } from '../../shared/data-access/supabase.service';
+
+interface NoteState {
+  notes: Note[];
+  loading: boolean;
+  error: boolean;
+}
+
+interface Note {
+  id: string;
+  title: string;
+  note: string | null;
+  color: string | null;
+  user_id: string;
+}
+
+@Injectable({ providedIn: 'root' })
+export class NotesService {
+  private _supabaseClient = inject(SupabaseService).supabaseClient;
+  private _state = signal<NoteState>({
+    notes: [],
+    loading: false,
+    error: false,
+  });
+
+  // selectors
+  notes = computed(() => this._state().notes);
+  loading = computed(() => this._state().loading);
+  error = computed(() => this._state().error);
+
+  async getAllNotes() {
+    try {
+      this._state.update((state) => ({
+        ...state,
+        loading: true,
+      }));
+      const { data } = await this._supabaseClient
+        .from('notes')
+        .select()
+        .returns<Note[]>();
+
+      if (data) {
+        this._state.update((state) => ({
+          ...state,
+          notes: data,
+        }));
+      }
+    } catch (error) {
+      this._state.update((state) => ({
+        ...state,
+        error: true,
+      }));
+    } finally {
+      this._state.update((state) => ({
+        ...state,
+        loading: false,
+      }));
+    }
+  }
+}
--- src/app/notes/features/note-list/note-list.component.ts
+++ src/app/notes/features/note-list/note-list.component.ts
@@ -1,6 +1,7 @@
-import { Component, inject } from '@angular/core';
+import { AfterViewInit, Component, inject } from '@angular/core';
 import { AuthService } from '../../../auth/data-access/auth.service';
 import { Router, RouterLink } from '@angular/router';
+import { NotesService } from '../../data-access/note.service';
 
 @Component({
   selector: 'app-note-list',
@@ -9,12 +10,17 @@ import { Router, RouterLink } from '@angular/router';
   templateUrl: './note-list.component.html',
   styles: ``,
 })
-export default class NoteListComponent {
+export default class NoteListComponent implements AfterViewInit {
   private _authService = inject(AuthService);
   private _router = inject(Router);
+  public notesService = inject(NotesService);
 
   async logOut() {
     await this._authService.singOut();
     this._router.navigateByUrl('/auth/log-in');
+  }
+
+  ngAfterViewInit(): void {
+    this.notesService.getAllNotes();
   }
 }
