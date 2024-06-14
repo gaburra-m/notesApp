09d2bab6d43531a366444451fb90d78d48a9fc80
Gaburra <gaburra.m@gmail.com>
2024-06-13 18:43:21 -0600

    Creacion del componente de alertas

--- src/app/auth/features/auth-sign-up/auth-sign-up.component.ts
+++ src/app/auth/features/auth-sign-up/auth-sign-up.component.ts
@@ -5,8 +5,9 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../data-access/auth.service';
+import { AlertComponent } from '../../../shared/components/alert/alert.component';
 
 interface SignUpForm {
   email: FormControl<null | string>;
@@ -16,13 +17,14 @@ interface SignUpForm {
 @Component({
   selector: 'app-auth-sign-up',
   standalone: true,
-  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './auth-sign-up.component.html',
   styles: ``,
+  imports: [RouterLink, ReactiveFormsModule, AlertComponent],
 })
 export default class AuthSignUpComponent {
   private _formBuilder = inject(FormBuilder);
   private _authService = inject(AuthService);
+  private _router = inject(Router);
 
   form = this._formBuilder.group<SignUpForm>({
     email: this._formBuilder.control(null, [
---
+++ src/app/shared/components/alert/alert.component.ts
@@ -0,0 +1,34 @@
+import { Component, input, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-alert',
+  standalone: true,
+  imports: [],
+  templateUrl: './alert.component.html',
+  styles: ``,
+})
+export class AlertComponent {
+  type = input<'success' | 'error' | 'warning' | 'info'>('info');
+  message = input<string>('');
+
+  get alertClasss() {
+    switch (this.type()) {
+      case 'success':
+        return 'text-green-800 bg-green-50 dark:bg-gray-800 dark:text-green-400';
+        break;
+
+      case 'error':
+        return ' text-red-800 bg-red-50 dark:bg-red-900 dark:text-red-100';
+        break;
+
+      case 'warning':
+        return 'text-yellow-800 bg-yellow-50 dark:bg-gray-800 dark:text-yellow-300';
+        break;
+
+      case 'info':
+      default:
+        return 'text-blue-800 bg-blue-50 dark:bg-gray-800 dark:text-blue-400';
+        break;
+    }
+  }
+}
